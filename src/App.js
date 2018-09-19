00d7c2b5279f768ef7dfd07cb4ef4bd219b5b778
Jesse Peterman <jessepeterman@gmail.com>
2018-09-18 21:04:02 -0700

    added genre selecter

--- src/App.js
+++ src/App.js
@@ -1,73 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import { callbackify } from 'util';
-
-// Create a WPM app
-// create a timer that goes from 60 seconds to 0 and stops
-// create a 3 2 1 countdown before starting timer
-// create a block of text that stores words
-// create reading display that will show 10 ( or so) words at a time
-// When user presses button (or right arrow) log the number of words just read
-// sum Tally the words read when the timer finishes
-// if time is set to something other than 60 seconds, divide the number of words by whatever to get WPM rate.
-//
-
-const PAPText = `It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife. However little known the feelings or views of such a man may be on his first entering a neighbourhood, this truth is so well fixed in the minds of the surrounding families, that he is considered the rightful property of some one or other of their daughters. “My dear Mr. Bennet,” said his lady to him one day, “have you heard that Netherfield Park is let at last?” Mr. Bennet replied that he had not. “But it is,” returned she; “for Mrs. Long has just been here, and she told me all about it.” Mr. Bennet made no answer. “Do you not want to know who has taken it?” cried his wife impatiently. “_You_ want to tell me, and I have no objection to hearing it.” This was invitation enough. “Why, my dear, you must know, Mrs. Long says that Netherfield is taken by a young man of large fortune from the north of England; that he came down on Monday in a chaise and four to see the place, and was so much delighted with it, that he agreed with Mr. Morris immediately; that he is to take possession before Michaelmas, and some of his servants are to be in the house by the end of next week.” “What is his name?” “Bingley.” “Is he married or single?” “Oh! Single, my dear, to be sure! A single man of large fortune; four or five thousand a year. What a fine thing for our girls!” “How so? How can it affect them? "My dear Mr. Bennet," replied his wife, "how can you be so tiresome! You
-must know that I am thinking of his marrying one of them."
-
-"Is that his design in settling here?"
-
-"Design! Nonsense, how can you talk so! But it is very likely that he
-_may_ fall in love with one of them, and therefore you must visit him as
-soon as he comes."
-
-"I see no occasion for that. You and the girls may go, or you may send
-them by themselves, which perhaps will be still better, for as you are
-as handsome as any of them, Mr. Bingley may like you the best of the
-party."
-
-"My dear, you flatter me. I certainly _have_ had my share of beauty, but
-I do not pretend to be anything extraordinary now. When a woman has five
-grown-up daughters, she ought to give over thinking of her own beauty."
-
-"In such cases, a woman has not often much beauty to think of."
-
-"But, my dear, you must indeed go and see Mr. Bingley when he comes into
-the neighbourhood."
-
-"It is more than I engage for, I assure you."
-
-"But consider your daughters. Only think what an establishment it would
-be for one of them. Sir William and Lady Lucas are determined to
-go, merely on that account, for in general, you know, they visit no
-newcomers. Indeed you must go, for it will be impossible for _us_ to
-visit him if you do not."
-
-"You are over-scrupulous, surely. I dare say Mr. Bingley will be very
-glad to see you; and I will send a few lines by you to assure him of my
-hearty consent to his marrying whichever he chooses of the girls; though
-I must throw in a good word for my little Lizzy."
-
-"I desire you will do no such thing. Lizzy is not a bit better than the
-others; and I am sure she is not half so handsome as Jane, nor half so
-good-humoured as Lydia. But you are always giving _her_ the preference."
-
-"They have none of them much to recommend them," replied he; "they are
-all silly and ignorant like other girls; but Lizzy has something more of
-quickness than her sisters."
-
-"Mr. Bennet, how _can_ you abuse your own children in such a way? You
-take delight in vexing me. You have no compassion for my poor nerves."
-
-"You mistake me, my dear. I have a high respect for your nerves. They
-are my old friends. I have heard you mention them with consideration
-these last twenty years at least."
-
-"Ah, you do not know what I suffer."
-
-"But I hope you will get over it, and live to see many young men of four
-thousand a year come into the neighbourhood."
-`;
+import fictionText from './PAPText';
+import { religiousText } from './PAPText';
+import { poetryText } from './PAPText';
 
 const TextDisplay = ({ text }) => {
   return <p id="text">{text || 'Prepare to read the text here'}</p>;
@@ -82,51 +17,44 @@ const Timer = props => {
   );
 };
 
-const PrintResults = props => {
+const TextSelector = props => {
+  const selectTextType = e => {
+    props.setTextType(e.target.value);
+  };
   return (
-    props.isFinished && (
-      <h2>You read {props.numOfWords * 4} words per minute.</h2>
-    )
+    <select onChange={selectTextType}>
+      <option>Select Genre</option>
+      <option>Fiction</option>
+      <option>Religious</option>
+      <option>Poetry</option>
+    </select>
   );
 };
 
+const defaultSettings = {
+  text: '',
+  textCounter: 0,
+  countdown: 59,
+  isRunning: false,
+  isFinished: false,
+  textType: 'Fiction',
+  fullText: fictionText
+};
+
 class WpmApp extends React.Component {
-  state = {
-    text: '',
-    textCounter: 0,
-    countdown: 14,
-    isRunning: false,
-    isFinished: false
-  };
+  state = defaultSettings;
 
   next = () => {
     if (!this.state.isRunning && !this.state.isFinished) {
       this.startTimer();
-
+      this.getNextText();
       this.setState({
         isRunning: true
       });
     } else if (!this.state.isFinished) {
-      let i = this.state.textCounter;
-      let end = this.state.textCounter + 10;
-      let nextText = '';
-      for (; i < end; i++) {
-        nextText += PAPText.split(' ')[i] + ' ';
-        this.setState({
-          text: nextText,
-          textCounter: this.state.textCounter + 10
-        });
-      }
+      this.getNextText();
     } else if (this.state.isFinished) {
-      setTimeout(() => {
-        this.setState({
-          text: '',
-          textCounter: 0,
-          countdown: 14,
-          isRunning: false,
-          isFinished: false
-        });
-      }, 3000);
+      this.resetAppState();
     }
   };
 
@@ -136,7 +64,6 @@ class WpmApp extends React.Component {
         return { countdown: prevstate.countdown - 1 };
       });
       if (this.state.countdown === 0) {
-        // alert(`You read ${this.state.textCounter * 4} words per minute!`);
         this.setState(prevstate => {
           return {
             isRunning: !prevstate.isRunning,
@@ -151,12 +78,60 @@ class WpmApp extends React.Component {
     }, 1000);
   };
 
+  getNextText = () => {
+    let i = this.state.textCounter;
+    let end = this.state.textCounter + 10;
+    let nextText = '';
+    let text = '';
+
+    if (i + 10 < this.state.fullText.replace(/\s+/g, ' ').split(' ').length) {
+      for (; i < end; i++) {
+        nextText +=
+          this.state.fullText.replace(/\s+/g, ' ').split(' ')[i] + ' ';
+        this.setState({
+          text: nextText,
+          textCounter: this.state.textCounter + 10
+        });
+      }
+    }
+  };
+
+  resetAppState = () => {
+    this.setState({
+      text: this.state.text + ' Resetting...'
+    });
+
+    setTimeout(() => {
+      this.setState(defaultSettings);
+    }, 3000);
+  };
+
+  setTextType = textType => {
+    this.setState({
+      textType
+    });
+    switch (textType) {
+      case 'Fiction':
+        this.setState({ fullText: fictionText });
+        console.log(this.state.textType);
+        break;
+      case 'Religious':
+        this.setState({ fullText: religiousText });
+        break;
+      case 'Poetry':
+        this.setState({ fullText: poetryText });
+        break;
+      default:
+        this.setState({ fullText: fictionText });
+    }
+  };
+
   printResults = () => {
     alert(`you read ${this.state.textCounter - 10} words`);
   };
 
   render() {
-    console.log(this.state);
+    console.log(this.state.textCounter);
     return (
       <div>
         <div className="container">
@@ -170,37 +145,11 @@ class WpmApp extends React.Component {
                 ? 'Reset'
                 : 'Start'}
           </div>
-
-          {/* <TestComponent /> */}
+          <TextSelector setTextType={this.setTextType} />
         </div>
       </div>
     );
   }
 }
-
-// class TestComponent extends React.Component {
-//   state = {
-//     timer: 5
-//   };
-
-//   handleClick = () => {
-//     // let timer = this.state.timer;
-//     // timer--;
-//     this.setState(prevstate => {
-//       return { timer: prevstate.timer - 1 };
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <h1>Test timer: {this.state.timer}</h1>
-//         <button onClick={this.handleClick}>Click</button>
-//       </div>
-//     );
-//   }
-// }
-
-// ReactDOM.render(<WpmApp />, document.querySelector("#app"));
 
 export default WpmApp;
---
+++ src/PAPText.js
@@ -0,0 +1,149 @@
+const fictionText = `It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife. However little known the feelings or views of such a man may be on his first entering a neighbourhood, this truth is so well fixed in the minds of the surrounding families, that he is considered the rightful property of some one or other of their daughters. “My dear Mr. Bennet,” said his lady to him one day, “have you heard that Netherfield Park is let at last?” Mr. Bennet replied that he had not. “But it is,” returned she; “for Mrs. Long has just been here, and she told me all about it.” Mr. Bennet made no answer. “Do you not want to know who has taken it?” cried his wife impatiently. “_You_ want to tell me, and I have no objection to hearing it.” This was invitation enough. “Why, my dear, you must know, Mrs. Long says that Netherfield is taken by a young man of large fortune from the north of England; that he came down on Monday in a chaise and four to see the place, and was so much delighted with it, that he agreed with Mr. Morris immediately; that he is to take possession before Michaelmas, and some of his servants are to be in the house by the end of next week.” “What is his name?” “Bingley.” “Is he married or single?” “Oh! Single, my dear, to be sure! A single man of large fortune; four or five thousand a year. What a fine thing for our girls!” “How so? How can it affect them? "My dear Mr. Bennet," replied his wife, "how can you be so tiresome! You
+must know that I am thinking of his marrying one of them."
+
+"Is that his design in settling here?"
+
+"Design! Nonsense, how can you talk so! But it is very likely that he
+_may_ fall in love with one of them, and therefore you must visit him as
+soon as he comes."
+
+"I see no occasion for that. You and the girls may go, or you may send
+them by themselves, which perhaps will be still better, for as you are
+as handsome as any of them, Mr. Bingley may like you the best of the
+party."
+
+"My dear, you flatter me. I certainly _have_ had my share of beauty, but
+I do not pretend to be anything extraordinary now. When a woman has five
+grown-up daughters, she ought to give over thinking of her own beauty."
+
+"In such cases, a woman has not often much beauty to think of."
+
+"But, my dear, you must indeed go and see Mr. Bingley when he comes into
+the neighbourhood."
+
+"It is more than I engage for, I assure you."
+
+"But consider your daughters. Only think what an establishment it would
+be for one of them. Sir William and Lady Lucas are determined to
+go, merely on that account, for in general, you know, they visit no
+newcomers. Indeed you must go, for it will be impossible for _us_ to
+visit him if you do not."
+
+"You are over-scrupulous, surely. I dare say Mr. Bingley will be very
+glad to see you; and I will send a few lines by you to assure him of my
+hearty consent to his marrying whichever he chooses of the girls; though
+I must throw in a good word for my little Lizzy."
+
+"I desire you will do no such thing. Lizzy is not a bit better than the
+others; and I am sure she is not half so handsome as Jane, nor half so
+good-humoured as Lydia. But you are always giving _her_ the preference."
+
+"They have none of them much to recommend them," replied he; "they are
+all silly and ignorant like other girls; but Lizzy has something more of
+quickness than her sisters."
+
+"Mr. Bennet, how _can_ you abuse your own children in such a way? You
+take delight in vexing me. You have no compassion for my poor nerves."
+
+"You mistake me, my dear. I have a high respect for your nerves. They
+are my old friends. I have heard you mention them with consideration
+these last twenty years at least."
+
+"Ah, you do not know what I suffer."
+
+"But I hope you will get over it, and live to see many young men of four
+thousand a year come into the neighbourhood."
+`;
+
+export const religiousText = `“Then we turned and journeyed into the wilderness in the direction of the Red Sea, as the LORD told me. And for many days we traveled around Mount Seir. Then the LORD said to me, ‘You have been traveling around this mountain country long enough. Turn northward and command the people, “You are about to pass through the territory of your brothers, the people of Esau, who live in Seir; and they will be afraid of you. So be very careful. Do not contend with them, for I will not give you any of their land, no, not so much as for the sole of the foot to tread on, because I have given Mount Seir to Esau as a possession. You shall purchase food from them with money, that you may eat, and you shall also buy water from them with money, that you may drink. For the LORD your God has blessed you in all the work of your hands. He knows your going through this great wilderness. These forty years the LORD your God has been with you. You have lacked nothing.”’ So we went on, away from our brothers, the people of Esau, who live in Seir, away from the Arabah road from Elath and Ezion-geber. “And we turned and went in the direction of the wilderness of Moab. And the LORD said to me, ‘Do not harass Moab or contend with them in battle, for I will not give you any of their land for a possession, because I have given Ar to the people of Lot for a possession.’ ( The Emim formerly lived there, a people great and many, and tall as the Anakim. Like the Anakim they are also counted as Rephaim, but the Moabites call them Emim. The Horites also lived in Seir formerly, but the people of Esau dispossessed them and destroyed them from before them and settled in their place, as Israel did to the land of their possession, which the LORD gave to them.) ‘Now rise up and go over the brook Zered.’ So we went over the brook Zered. And the time from our leaving Kadesh-barnea until we crossed the brook Zered was thirty-eight years, until the entire generation, that is, the men of war, had perished from the camp, as the LORD had sworn to them. For indeed the hand of the LORD was against them, to destroy them from the camp, until they had perished. “So as soon as all the men of war had perished and were dead from among the people, the LORD said to me, ‘Today you are to cross the border of Moab at Ar. And when you approach the territory of the people of Ammon, do not harass them or contend with them, for I will not give you any of the land of the people of Ammon as a possession, because I have given it to the sons of Lot for a possession.’ (It is also counted as a land of Rephaim. Rephaim formerly lived there—but the Ammonites call them Zamzummim— a people great and many, and tall as the Anakim; but the LORD destroyed them before the Ammonites, and they dispossessed them and settled in their place, as he did for the people of Esau, who live in Seir, when he destroyed the Horites before them and they dispossessed them and settled in their place even to this day. As for the Avvim, who lived in villages as far as Gaza, the Caphtorim, who came from Caphtor, destroyed them and settled in their place.) ‘Rise up, set out on your journey and go over the Valley of the Arnon. Behold, I have given into your hand Sihon the Amorite, king of Heshbon, and his land. Begin to take possession, and contend with him in battle. This day I will begin to put the dread and fear of you on the peoples who are under the whole heaven, who shall hear the report of you and shall tremble and be in anguish because of you.’ “So I sent messengers from the wilderness of Kedemoth to Sihon the king of Heshbon, with words of peace, saying, ‘Let me pass through your land. I will go only by the road; I will turn aside neither to the right nor to the left. You shall sell me food for money, that I may eat, and give me water for money, that I may drink. Only let me pass through on foot, as the sons of Esau who live in Seir and the Moabites who live in Ar did for me, until I go over the Jordan into the land that the LORD our God is giving to us.’ But Sihon the king of Heshbon would not let us pass by him, for the LORD your God hardened his spirit and made his heart obstinate, that he might give him into your hand, as he is this day. And the LORD said to me, ‘Behold, I have begun to give Sihon and his land over to you. Begin to take possession, that you may occupy his land.’ Then Sihon came out against us, he and all his people, to battle at Jahaz. And the LORD our God gave him over to us, and we defeated him and his sons and all his people. And we captured all his cities at that time and devoted to destruction every city, men, women, and children. We left no survivors. Only the livestock we took as spoil for ourselves, with the plunder of the cities that we captured. From Aroer, which is on the edge of the Valley of the Arnon, and from the city that is in the valley, as far as Gilead, there was not a city too high for us. The LORD our God gave all into our hands. Only to the land of the sons of Ammon you did not draw near, that is, to all the banks of the river Jabbok and the cities of the hill country, whatever the LORD our God had forbidden us.`;
+
+export const poetryText = `Gerontion
+
+       Thou hast nor youth nor age
+       But as it were an after dinner sleep
+       Dreaming of both.
+
+     Here I am, an old man in a dry month,
+     Being read to by a boy, waiting for rain.
+     I was neither at the hot gates
+     Nor fought in the warm rain
+     Nor knee deep in the salt marsh, heaving a cutlass,
+     Bitten by flies, fought.
+     My house is a decayed house,
+     And the jew squats on the window sill, the owner,
+     Spawned in some estaminet of Antwerp,
+     Blistered in Brussels, patched and peeled in London.
+     The goat coughs at night in the field overhead;
+     Rocks, moss, stonecrop, iron, merds.
+     The woman keeps the kitchen, makes tea,
+     Sneezes at evening, poking the peevish gutter.
+
+                       I an old man,
+     A dull head among windy spaces.
+
+     Signs are taken for wonders. "We would see a sign":
+     The word within a word, unable to speak a word,
+     Swaddled with darkness. In the juvescence of the year
+     Came Christ the tiger
+
+     In depraved May, dogwood and chestnut, flowering Judas,
+     To be eaten, to be divided, to be drunk
+     Among whispers; by Mr. Silvero
+     With caressing hands, at Limoges
+     Who walked all night in the next room;
+     By Hakagawa, bowing among the Titians;
+     By Madame de Tornquist, in the dark room
+     Shifting the candles; Fraulein von Kulp
+     Who turned in the hall, one hand on the door. Vacant shuttles
+     Weave the wind. I have no ghosts,
+     An old man in a draughty house
+     Under a windy knob.
+
+     After such knowledge, what forgiveness? Think now
+     History has many cunning passages, contrived corridors
+     And issues, deceives with whispering ambitions,
+     Guides us by vanities. Think now
+     She gives when our attention is distracted
+     And what she gives, gives with such supple confusions
+     That the giving famishes the craving. Gives too late
+     What's not believed in, or if still believed,
+     In memory only, reconsidered passion. Gives too soon
+     Into weak hands, what's thought can be dispensed with
+     Till the refusal propagates a fear. Think
+     Neither fear nor courage saves us. Unnatural vices
+     Are fathered by our heroism. Virtues
+     Are forced upon us by our impudent crimes.
+     These tears are shaken from the wrath-bearing tree.
+
+     The tiger springs in the new year. Us he devours. Think at last
+     We have not reached conclusion, when I
+     Stiffen in a rented house. Think at last
+     I have not made this show purposelessly
+     And it is not by any concitation
+     Of the backward devils.
+     I would meet you upon this honestly.
+     I that was near your heart was removed therefrom
+     To lose beauty in terror, terror in inquisition.
+     I have lost my passion: why should I need to keep it
+     Since what is kept must be adulterated?
+     I have lost my sight, smell, hearing, taste and touch:
+     How should I use it for your closer contact?
+
+     These with a thousand small deliberations
+     Protract the profit of their chilled delirium,
+     Excite the membrane, when the sense has cooled,
+     With pungent sauces, multiply variety
+     In a wilderness of mirrors. What will the spider do,
+     Suspend its operations, will the weevil
+     Delay? De Bailhache, Fresca, Mrs. Cammel, whirled
+     Beyond the circuit of the shuddering Bear
+     In fractured atoms. Gull against the wind, in the windy straits
+     Of Belle Isle, or running on the Horn,
+     White feathers in the snow, the Gulf claims,
+     And an old man driven by the Trades
+     To a sleepy corner.
+
+                       Tenants of the house,
+     Thoughts of a dry brain in a dry season.`;
+
+export default fictionText;
